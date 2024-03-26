7be80ec4ae9d959ba0d0dc147c574d10dece9708
Lucas Pineda <lucas.pineda@hotmail.com>
2024-03-26 16:20:24 -0300

    create home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
 import Image from "next/image";
+// import PeopleCookingImg from "./../public/people-cooking.svg"
 
 export default function Home() {
   return (
-    <div className="bg-primary">hii</div>
+    <div className="bg-primary flex justify-center">
+      <Image src="/yellow-wave.svg" width={340} height={180} alt="test" className="absolute left-0"/>
+      <Image src="/people-cooking.svg" width={250} height={200} alt="test" className="mt-28 z-10"/>
+      <h1>ChefinhoIA</h1>
+      <div>
+        <h2>
+			Utilize ingredientes que
+			possuí em casa para criar
+			receitas incríveis
+        </h2>
+      </div>
+    </div>
   );
 }
