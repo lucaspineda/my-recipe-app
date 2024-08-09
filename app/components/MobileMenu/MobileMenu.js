61c8e3b69368899479e43d43a060a6908c49d652
Lucas Pineda <lucas.pineda@hotmail.com>
2024-08-08 21:31:01 -0300

    add onclick to links to close menu

--- app/components/MobileMenu/MobileMenu.js
+++ app/components/MobileMenu/MobileMenu.js
@@ -28,22 +28,34 @@ export default function MobileMenu({ toggleMenu }) {
         <h2 className="mt-4 text-lg font-semibold">Lucas Pineda</h2>
         <span>Plano: Premium</span>
       </div>
-      <nav className="mt-8" onClick={toggleMenu}>
+      <nav className="mt-8">
         <ul className="flex flex-col gap-8">
           <li>
-            <Link className="flex items-center gap-1 text-black no-underline" href="/">
+            <Link
+              className="flex items-center gap-1 text-black no-underline"
+              href="/"
+              onClick={toggleMenu}
+            >
               <IconToolsKitchen2 size={20} stroke={2} />
               Criar Receitas
             </Link>
           </li>
           <li>
-            <Link className="flex items-center gap-1 text-black no-underline" href="/">
+            <Link
+              className="flex items-center gap-1 text-black no-underline"
+              href="/plans"
+              onClick={toggleMenu}
+            >
               <IconShoppingBag size={20} stroke={2} />
               Meu Plano
             </Link>
           </li>
           <li>
-            <Link className="flex items-center gap-1 text-black no-underline" href="/">
+            <Link
+              className="flex items-center gap-1 text-black no-underline"
+              href="/"
+              onClick={toggleMenu}
+            >
               <IconUser size={20} stroke={2} />
               Perfil
             </Link>
