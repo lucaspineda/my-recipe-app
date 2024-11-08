a187d3d6b737f0673ca9c24bcf51645dab07d11f
lucaspineda <lucas.pineda@hotmail.com>
2024-11-08 15:29:02 -0300

    use tsx for login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,7 @@ import { useUserAuth } from "../hooks/userAuth";
 
 const schema = z.object({
   email: z.string().email("Email é obrigatório"),
-  password: z.string("Senha é obrigatório").min(1, "Senha é obrigatório")
+  password: z.string("Senha é obrigatório").min(1, "Senha é obrigatório"),
 });
 
 export default function Login() {
@@ -19,7 +19,7 @@ export default function Login() {
   const [signInError, setSignInError] = useState();
 
   const router = useRouter();
-  const {signInWithEmail, error} = useUserAuth()
+  const { signInWithEmail, error } = useUserAuth();
   const auth = getAuth();
 
   const {
@@ -39,26 +39,28 @@ export default function Login() {
   }, [auth, router]);
 
   useEffect(() => {
-    console.log(errors)
+    console.log(errors);
   }, [errors]);
-  
 
   const handleSignInWithEmail = () => {
-    signInWithEmail(email, password)
+    signInWithEmail(email, password);
   };
 
   const handleKeyDown = (e) => {
-    if(e.key === "enter") {
-      handleSubmit(handleSignInWithEmail)
+    if (e.key === "enter") {
+      handleSubmit(handleSignInWithEmail);
     }
-  }
+  };
 
   return (
     <main className="flex flex-col text-center h-screen justify-center">
       <div>
         <h1 className="text-2xl">Bem-Vindo ao Chefinho IA</h1>
         <h2 className="my-6">Faça seu login</h2>
-        <form onSubmit={handleSubmit(handleSignInWithEmail)} className="flex flex-col justify-start items-start">
+        <form
+          onSubmit={handleSubmit(handleSignInWithEmail)}
+          className="flex flex-col justify-start items-start"
+        >
           <input
             {...register("email")}
             className="global-input"
@@ -69,7 +71,11 @@ export default function Login() {
             onKeyDown={handleKeyDown}
             onChange={(e) => setEmail(e.target.value)}
           />
-          {errors?.email?.message && <span className="text-red-700 text-sm m-2">{errors?.email?.message}</span>}
+          {errors?.email?.message && (
+            <span className="text-red-700 text-sm m-2">
+              {errors?.email?.message}
+            </span>
+          )}
           <input
             {...register("password")}
             className="global-input mt-2"
@@ -80,8 +86,14 @@ export default function Login() {
             onKeyDown={handleKeyDown}
             onChange={(e) => setPassword(e.target.value)}
           />
-          {errors?.password?.message && <span className="text-red-700 text-sm m-2">{errors?.password?.message}</span>}
-          {signInError && <span className="text-red-700 text-sm m-2">{signInError}</span>}
+          {errors?.password?.message && (
+            <span className="text-red-700 text-sm m-2">
+              {errors?.password?.message}
+            </span>
+          )}
+          {signInError && (
+            <span className="text-red-700 text-sm m-2">{signInError}</span>
+          )}
           <span className="text-xs mt-2 text-left">
             <Link href="/password-reset">Esqueceu a senha?</Link>
           </span>
