b2624b7bdf0885e5fa64baaaefe31a6f9c185ed5
lucaspineda <lucas.pineda@hotmail.com>
2024-12-30 01:54:01 -0300

    handle empty user

--- app/components/MobileMenu/MobileMenu.tsx
+++ app/components/MobileMenu/MobileMenu.tsx
@@ -23,6 +23,8 @@ export default function MobileMenu({ toggleMenu }) {
       console.log("user is signed out");
     });
   };
+
+  if (!user) return null;
   return (
     <div className="mobile-menu flex flex-col fixed w-screen h-screen bg-black z-10">
       <Image
