2054fd94c64da7d41c943e33e4e9450341bade82
jefferson.willian <jefferson.lsilva@me.com.br>
2025-01-13 17:37:39 -0300

    new icon whatsapp

--- app/components/WhatsApp/WhatsApp.jsx
+++ app/components/WhatsApp/WhatsApp.jsx
@@ -6,10 +6,10 @@ const WhatsAppButton = () => {
       href="https://wa.me/5511976783992"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-5 right-5 z-50 w-24 h-24 bg-green-500 rounded-full flex items-center justify-center shadow-lg hover:bg-green-600 transition-transform transform hover:scale-110"
+      className="fixed bottom-5 right-5 z-50 w-20 h-20 bg-green-500 rounded-full flex items-center justify-center shadow-lg hover:bg-green-600 transition-transform transform hover:scale-110"
     >
       <Image
-        src="/images/whats-app-white.svg"
+        src="/images/icon-whatsapp.svg"
         alt="WhatsApp"
         width={68} 
         height={40} 
