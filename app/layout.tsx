72cbf417dfaebc26050c21bbbb82a7fe173cda54
lucaspineda <lucas.pineda@hotmail.com>
2025-05-09 23:06:02 -0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,12 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍🍳</text></svg>`}
+        />
+      </head>
       <body className={rubik.className}>
         <ProtectedRoute onSetUser={(user) => onSetUser(user)}>
           <ToastContainer
