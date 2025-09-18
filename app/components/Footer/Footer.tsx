706c4a773b7dfd3508171429eb37339c6cf024ce
lucaspineda <lucas.pineda@hotmail.com>
2025-09-18 10:52:11 -0300

    fix footer margin

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="bg-secondary mt-20">
+    <footer className="bg-secondary mt-auto">
       <div className="max-w-6xl mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center">
           <div className="text-center md:text-left">
